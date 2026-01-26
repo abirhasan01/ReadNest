e8dec923e1468880be8e4f83bc5878382c30ec28
abirhasan01 <appleabirhasan@gmail.com>
2026-01-26 07:59:03 -0800

    fixed header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import Blogs from "../components/Blogs";
-import Header from "../components/header";
+import Head from "../components/Head";
 import NewsLetterBox from "../components/NewsLetterBox";
 
 const Home = () => {
   return (
     <div className="px-5 sm:px-[1vw] md:px-[3vw] lg:px-[5vw]">
-      <Header />
+      <Head />
       <Blogs />
       <NewsLetterBox />
     </div>
